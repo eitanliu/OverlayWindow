a7d1b1c7bfd6a5870e4f18ca5a6771fe64a93a79
eitanliu <eitanliu@gmail.com>
2025-08-11 18:20:17 +0800

    example

--- app/src/main/java/com/example/overlay/MainActivity.kt
+++ app/src/main/java/com/example/overlay/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.overlay
 
+import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.eitanliu.overlay.OverlayWindow
 import com.example.overlay.databinding.ActivityMainBinding
@@ -28,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContentView(R.layout.activity_main)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -36,6 +39,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         binding.button.setOnClickListener {
+            fixWindow()
             leftTopWindow()
             rightBottomWindow()
         }
@@ -45,8 +49,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun fixWindow() {
+        val window = window
+        val decorView = window.decorView
+        val layoutParams = decorView.layoutParams
+        (layoutParams as? WindowManager.LayoutParams)?.apply {
+            // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            //     layoutInDisplayCutoutMode = WindowManager.LayoutParams
+            //         .LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+            // }
+        }
+        window.windowManager.updateViewLayout(decorView, decorView.layoutParams)
+    }
+
     private fun leftTopWindow() {
         val ltWindow = OverlayWindow(View(this), windowManager)
+        val decorView = ltWindow.view
+        decorView.rootWindowInsets
+
+        val decorFitsSystemWindows = true
+        // val stableFlag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        // val stableFlag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+        //         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+        //         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        // val sysUiVis = decorView.getSystemUiVisibility()
+        // decorView.setSystemUiVisibility(
+        //     if (decorFitsSystemWindows)
+        //         sysUiVis and stableFlag.inv()
+        //     else sysUiVis or stableFlag
+        // )
         ltWindow.view.setBackgroundColor(AREA_VIEW_COLOR)
         ltWindow.updateLayoutParams {
             width = AREA_VIEW_SIZE
@@ -56,12 +87,19 @@ class MainActivity : AppCompatActivity() {
                     // WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
                     WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                layoutInDisplayCutoutMode = WindowManager.LayoutParams
-                    .LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                layoutInDisplayCutoutMode = WindowManager.LayoutParams
-                    .LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            if (decorFitsSystemWindows) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    layoutInDisplayCutoutMode = WindowManager.LayoutParams
+                        .LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+                }
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    layoutInDisplayCutoutMode = WindowManager.LayoutParams
+                        .LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+                } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    layoutInDisplayCutoutMode = WindowManager.LayoutParams
+                        .LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+                }
             }
         }
         ltWindow.attachView()
@@ -81,5 +119,9 @@ class MainActivity : AppCompatActivity() {
                     WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         }
         rbWindow.attachView()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
     }
 }
--- app/src/main/java/com/example/overlay/TestActivity.kt
+++ app/src/main/java/com/example/overlay/TestActivity.kt
@@ -1,15 +1,19 @@
 package com.example.overlay
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.example.overlay.databinding.ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
 
+    private var fitWindow = true
     val binding by lazy {
         ActivityTestBinding.inflate(layoutInflater)
     }
@@ -41,5 +45,45 @@ class TestActivity : AppCompatActivity() {
                 windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
             }
         }
+        binding.btnFitWindow.setOnClickListener {
+            fitWindow = !fitWindow
+            fitWindow()
+        }
+    }
+
+    private fun fitWindow() {
+        // WindowCompat.setDecorFitsSystemWindows(window, fitWindow)
+        fitWindow16()
+    }
+
+    private fun fitWindow35() {
+        // windowInsetsController.systemBarsBehavior =
+        //     WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(fitWindow)
+        }
+    }
+
+    private fun fitWindow30() {
+        val decorView = window.decorView
+        val maskFlag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        val sysUiVis = decorView.getSystemUiVisibility()
+        decorView.setSystemUiVisibility(
+            if (fitWindow) sysUiVis and maskFlag.inv() else sysUiVis or maskFlag
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(fitWindow)
+        }
+    }
+
+    private fun fitWindow16() {
+        val decorView = window.decorView
+        val maskFlag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        val sysUiVis = decorView.getSystemUiVisibility()
+        decorView.setSystemUiVisibility(
+            if (fitWindow) sysUiVis and maskFlag.inv() else sysUiVis or maskFlag
+        )
     }
 }
